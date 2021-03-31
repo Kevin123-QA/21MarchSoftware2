7335cee28f986da7fed18ca0a720635b816cde46
Kevin123-QA <KTse@qa.com>
2021-03-31 15:32:13 +0100

    Completed exams exercise

--- src/com/qa/day1/Exams.java
+++ src/com/qa/day1/Exams.java
@@ -1,5 +1,25 @@
 package com.qa.day1;
 
 public class Exams {
-//testing git
+	static int physics;
+	static int chemistry;
+	static int biology;
+	static int total;
+	static int percentage;
+
+	public static void displayResults() {
+		total = physics + chemistry + biology;
+		
+		String result = "PHYSICS: " + physics + "\n" // \n new line charachter
+					+ "CHEMISTRY:" + chemistry + "\n" 
+				+"BIOLOGY: " + biology + "\n"
+				+ "TOTLA:" + total + "/450";
+		System.out.println(result);
+	}
+	
+	public static void displayTotalAsPercentage() {
+		total = physics + chemistry + biology;
+		
+		System.out.println("PERCENTAGE: " + (total*100/450) + "%");
+	}
 }
---
+++ src/com/qa/day1/Runner.java
@@ -0,0 +1,12 @@
+package com.qa.day1;
+
+public class Runner {
+	public static void main(String[] args) {
+		Exams.biology = 90;
+				Exams.chemistry=90;
+				Exams.physics = 90;
+				
+				Exams.displayResults();
+		Exams.displayTotalAsPercentage();
+	}
+}
